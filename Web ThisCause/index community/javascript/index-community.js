5144e5656deda1b3ba647e8a45b33b8317f89eca
Fista Puteri Adna <165313449+FstFista30@users.noreply.github.com>
2024-04-15 11:12:29 +0700

    Add files via upload

---
+++ Web ThisCause/index community/javascript/index-community.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 2.7,
+    spaceBetween: 5,
+    freeMode: true,
+    centerSlide : 'true',
+    fade:'true',
+    grabCursor:'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets:true,
+    },
+  });
